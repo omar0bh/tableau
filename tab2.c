36d9274a2154e0fd955fd6177e3593f1f250d33d
omar0bh <162894760+omar0bh@users.noreply.github.com>
2024-12-12 22:33:08 +0100

    Add files via upload

---
+++ tab2.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+void entre(int a , int tab[]) {
+for(int i =0; i <a;i++) {
+printf("entre la valeur de taille : ");
+    scanf("%d",&tab[i]);
+         }
+}
+void sort(int a, int tab[]) {
+    for (int j =0;j<a;j++) {
+        printf("tab[%d] = %d\n",j ,tab[j]);
+    }
+}
+int main () {
+    int a;
+    int tab[a];
+    int q=1;
+    while(q==1) {
+        do {
+            printf("donner la taille du tableau :");
+            scanf("%d",&a);
+            if (a <= 0) {
+                printf("taille invalide.\n");
+            }
+        }while (a<=0);
+        entre(a , tab);
+        sort(a,tab);
+        printf("\n voulez-vous redemarer le programme ? \n");
+        printf("tapez 1 pour redemarer ou 0 pour quitter : ");
+        scanf("%d",&q);
+
+        if ((q!=1) && (q!=0)) {
+            printf("erreur !  entrer (1 pour redemarer ou 0 pour quitter) \n");
+        }
+    }
+    return 0;
+}
+
+
+
+
+    /*
+
+for (int i =0;i<4;i++) {
+    printf("\n tab[%d]=%d",i,tab[i]);
+}
+
+    printf("\n la valeur de la deuxieme case est  : %d",tab[0]);
+    printf("\n la valeur de la deuxieme case est  : %d",tab[3]);
+*/
+
+
